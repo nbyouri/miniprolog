82c079a7c17b92934b5de4abac3ad9db95cc2b25
Youri Mouton <youri@NetBSD.org>
2017-05-11 15:20:16 +0200

    Add some tests

--- miniprolog/src/miniprolog/core.clj
+++ miniprolog/src/miniprolog/core.clj
@@ -12,8 +12,10 @@
   (Character/isUpperCase (first (str x))))
 
 ;; detect a symbol
-(defn issym [x]
-  (not (isvar x)))
+(defn isatom [x]
+  (and
+       (not (isvar x))
+       (not (seq? x))))
 
 ;; get facts/rules from the database
 (defn getdb [data]
@@ -27,16 +29,6 @@
 (defn rulebody [rule]
   (rest rule))
 
-;; define new rules and facts (& is for varargs)
-(defmacro <- [& clause]
-  `(addtodb '~clause))
-
-(defmacro ?- [rule]
-  (println "RULE --> " rule)
-  (let [rules (findrules [] (->Goal rule [rule] {} nil) rule {})]
-    (println "QUERY --> " rules)
-    `(interpret #{} '~rules)))
-
 ;; rule to prolog predicate name and arity (father X Y) -> [father 2]
 (defn rulekey [rule]
   (if (seq? (first rule))
@@ -48,63 +40,25 @@
   (dosync
     (get @*db* rules)))
 
-;; add rule to the database
+;; append rules
 (defn addrule [rule]
     (update @*db* (rulekey rule) (fnil conj #{}) (if (seq? (first rule))
                                                    rule
                                                    (list rule))))
-
+;; update rule db
 (defn addtodb [rule]
   (let [rules (addrule rule)]
     (dosync
       (ref-set *db* rules))))
 
+;; record of goals
 (defrecord Goal [head terms env parent])
-
-(defn findrules [queue parent term env]
-  (reduce
-    (fn [acc rule]
-      (if-let [ans (unify term env (first rule) {})]
-        (conj acc (->Goal (first rule) (next rule) ans parent))
-        acc))
-    queue
-    (*db* (rulekey term))))
-
-(defn interpret [acc queue]
-  (println "queue " queue)
-  (let [{:keys [head terms env parent] :as goal} (peek queue)]
-    (println "goal " (:head goal))
-    (if-not goal
-      acc
-      (let [queue (pop queue)]
-        (println "let queue " queue)
-        (if-not terms
-          (if-not parent
-            (let [acc (conj acc env)]
-              (if (reduced? acc)     
-                acc
-                (recur acc queue)))
-            (let [parent-env (unify head env (-> parent :terms first) (:env parent))]
-              (recur acc (conj queue (assoc parent :env parent-env
-                                       :terms (-> parent :terms next))))))
-          (let [term  (first terms)
-                queue (findrules queue goal term env)]
-            (recur acc queue)))))))
-
-
-(defn lvar? [x]
-  (symbol? x))
-
-(defn constant? [x]
-  (and
-       (not (lvar? x))
-       (not (seq? x))))
 
 ;; Solve a query with it's environment
 (defn solve [src src-env]
   (cond
-    (constant? src) src
-    (lvar? src) (when-let [ans (get src-env src)]
+    (isatom src) src
+    (isvar src) (when-let [ans (get src-env src)]
                   (recur ans src-env))
     :else (let [[head & args] src
                 _ (assert (symbol? head) "Error")
@@ -118,20 +72,20 @@
             (when rargs
               (cons head rargs)))))
                                 
-                  
+;; unification of two queries and their environment                  
 (defn unify [src src-env dest dest-env]
   (assert (and (not (nil? src))
                (not (nil? dest))))
   (cond
-    (and (constant? src)
-         (constant? dest)) (and (= src dest)
-                                dest-env)
+    (and (isatom src)
+         (isatom dest)) (and (= src dest)
+                             dest-env)
     
-    (lvar? src) (if-let [resolved (solve src src-env)]
+    (isvar src) (if-let [resolved (solve src src-env)]
                   (recur resolved src-env dest dest-env)
                   dest-env)
     
-    (lvar? dest) (if-let [resolved (solve dest dest-env)]
+    (isvar dest) (if-let [resolved (solve dest dest-env)]
                    (recur src src-env resolved dest-env)
                    (assoc dest-env dest (solve src src-env)))
     
@@ -151,9 +105,63 @@
                   (recur new-env (next src-seq) (next dest-seq))))
               dest-env))))
 
+;; find a rule based on predicate name and arity
+(defn findrules [queue parent term env]
+  (reduce
+    (fn [acc rule]
+      (if-let [ans (unify term env (first rule) {})]
+        (conj acc (->Goal (first rule) (next rule) ans parent))
+        acc))
+    queue
+    (*db* (rulekey term))))
+
+
+;; interpret a set of goals
+(defn interpret [acc queue]
+  ;(println "queue " queue)
+  (let [{:keys [head terms env parent] :as goal} (peek queue)]
+    ;(println "goal " (:head goal))
+    (if-not goal
+      acc
+      (let [queue (pop queue)]
+        ;(println "let queue " queue)
+        (if-not terms
+          (if-not parent
+            (let [acc (conj acc env)]
+              (if (reduced? acc)     
+                acc
+                (recur acc queue)))
+            (let [parent-env (unify head env (-> parent :terms first) (:env parent))]
+              (recur acc (conj queue (assoc parent :env parent-env
+                                       :terms (-> parent :terms next))))))
+          (let [term  (first terms)
+                queue (findrules queue goal term env)]
+            (recur acc queue)))))))
 
 
 
 
+;; define new rules and facts
+(defmacro <- [& clause]
+  `(addtodb '~clause))
+
+;; apply a query to the database
+(defn query [acc query]
+  ;(println "INPUT " query)
+  (let [rule (first query)]
+    ;(println "RULE --> " rule)
+    (if-not rule
+      ; Return the first and only element of the list
+      (first acc)
+      (let [rules (findrules [] (->Goal rule [rule] {} nil) rule {})]
+        ;(println "QUERY --> " rules)
+        (let [result (conj acc (interpret #{} rules))]
+          (recur result (rest query)))))))
+          
+
+
+;; query macro
+(defmacro ?- [& query]
+  `(query nil '~query))
 
 
--- miniprolog/test/miniprolog/core_test.clj
+++ miniprolog/test/miniprolog/core_test.clj
@@ -6,4 +6,96 @@
   (testing "Couldn't find out whether symbol is a prolog variable or an atom"
     (is (= true (isvar "Type")))))
 
-(deftest )
+(deftest simplequery
+  (testing "simple query")
+  (<- (father george maria))
+  (<- (father george howard))
+  (<- (father george roger))
+  (<- (father george laura))
+  (<- (father albert tamara))
+  (<- (father albert alexandra))
+  (<- (father albert jessica))
+  (<- (father roger brandon))
+  (<- (father roger nadia))
+  (<- (father bob frank))
+  (<- (father bob anthony))
+
+  (<- (mother cecilia howard))
+  (<- (mother cecilia roger))
+  (<- (mother cecilia laura))
+  (<- (mother maria tamara))
+  (<- (mother maria alexandra))
+  (<- (mother maria jessica))
+  (<- (mother sarah brandon))
+  (<- (mother sarah nadia))
+  (<- (mother laura frank))
+  (<- (mother laura anthony))
+
+  (<- (male george))
+  (<- (male albert))
+  (<- (male roger))
+  (<- (male howard))
+  (<- (male bob))
+  (<- (male brandon))
+  (<- (male frank))
+  (<- (male anthony))
+
+  (<- (female cecilia))
+  (<- (female maria))
+  (<- (female sarah))
+  (<- (female laura))
+  (<- (female tamara))
+  (<- (female alexandra))
+  (<- (female jessica))
+  (<- (female nadia))
+  
+  (<- (parent Parent Child) (father Parent Child))
+  (<- (parent Parent Child) (mother Parent Child))
+  
+  (<- (ancestor X Z) (parent X Z))
+  (<- (ancestor X Z) (parent X Y) (ancestor Y Z))
+  
+  (<- (append (list) T T))
+  (<- (append (list H T) L2 (list H TR))
+      (append T L2 TR))
+  
+  (is (= (?- (male X))
+         '#{{X george}
+            {X albert}
+            {X roger}
+            {X howard}
+            {X bob}
+            {X brandon}
+            {X frank}
+            {X anthony}}))
+  
+  (is (= (?- (father george X))
+         '#{{X maria}
+            {X howard}
+            {X roger}
+            {X laura}}))
+
+  (is (= (?- (father X Y))
+         '#{{X george Y maria}
+            {X george Y howard}
+            {X george Y roger}
+            {X george Y laura}
+            {X albert Y tamara}
+            {X albert Y alexandra}
+            {X albert Y jessica}
+            {X roger Y brandon}
+            {X roger Y nadia}
+            {X bob Y frank}
+            {X bob Y anthony}}))
+  
+  (is (= (?- (ancestor X tamara))
+         '#{{X maria}
+            {X albert}
+            {X george}})))
+
+ 
+
+
+         
+  
+ 
